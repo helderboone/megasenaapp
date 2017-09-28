da973bfe936545d33c796eed887716b817ad42e1
Helder Boone <helder.boone@gmail.com>
2017-09-27 21:37:30 -0300

    Cria o contrato para reutilizacao na lotomania

---
+++ LoterySefaz/LoterySefaz.Core/IDraw.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace LoterySefaz.Core
+{
+    public interface IDraw
+    {
+        Dictionary<string, string> Draw(Player player);
+
+        List<int> DrawnNumbers { get; set; }
+    }
+}
