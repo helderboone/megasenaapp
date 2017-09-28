62309e4e193f883899108167999aec91792d9364
Helder Boone <helder.boone@gmail.com>
2017-09-28 18:38:18 -0300

    Implementa a tela para selecionar os numeros da mega sena

---
+++ LoterySefaz/LoterySefaz/NumbersChosen.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace LoterySefaz
+{
+    partial class NumbersChosen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdNumbers = new System.Windows.Forms.DataGridView();
+            this.clm1Check = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.clmText2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCheck2 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.clmText1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCheck3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.clmText3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCheck4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.clmText4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCheck5 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.clmText5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCheck6 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.clmText6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAnotherBet = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdNumbers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdNumbers
+            // 
+            this.grdNumbers.AllowUserToAddRows = false;
+            this.grdNumbers.AllowUserToDeleteRows = false;
+            this.grdNumbers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grdNumbers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdNumbers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clm1Check,
+            this.clmText2,
+            this.clmCheck2,
+            this.clmText1,
+            this.clmCheck3,
+            this.clmText3,
+            this.clmCheck4,
+            this.clmText4,
+            this.clmCheck5,
+            this.clmText5,
+            this.clmCheck6,
+            this.clmText6});
+            this.grdNumbers.Location = new System.Drawing.Point(29, 63);
+            this.grdNumbers.Name = "grdNumbers";
+            this.grdNumbers.Size = new System.Drawing.Size(398, 242);
+            this.grdNumbers.TabIndex = 0;
+            this.grdNumbers.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdNumbers_CellValueChanged);
+            this.grdNumbers.CurrentCellDirtyStateChanged += new System.EventHandler(this.grdNumbers_CurrentCellDirtyStateChanged);
+            // 
+            // clm1Check
+            // 
+            this.clm1Check.FillWeight = 5F;
+            this.clm1Check.HeaderText = "";
+            this.clm1Check.Name = "clm1Check";
+            // 
+            // clmText2
+            // 
+            this.clmText2.FillWeight = 5F;
+            this.clmText2.HeaderText = "";
+            this.clmText2.Name = "clmText2";
+            this.clmText2.ReadOnly = true;
+            // 
+            // clmCheck2
+            // 
+            this.clmCheck2.FillWeight = 5F;
+            this.clmCheck2.HeaderText = "";
+            this.clmCheck2.Name = "clmCheck2";
+            this.clmCheck2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.clmCheck2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // clmText1
+            // 
+            this.clmText1.FillWeight = 5F;
+            this.clmText1.HeaderText = "";
+            this.clmText1.Name = "clmText1";
+            this.clmText1.ReadOnly = true;
+            // 
+            // clmCheck3
+            // 
+            this.clmCheck3.FillWeight = 5F;
+            this.clmCheck3.HeaderText = "";
+            this.clmCheck3.Name = "clmCheck3";
+            // 
+            // clmText3
+            // 
+            this.clmText3.FillWeight = 5F;
+            this.clmText3.HeaderText = "";
+            this.clmText3.Name = "clmText3";
+            this.clmText3.ReadOnly = true;
+            // 
+            // clmCheck4
+            // 
+            this.clmCheck4.FillWeight = 5F;
+            this.clmCheck4.HeaderText = "";
+            this.clmCheck4.Name = "clmCheck4";
+            // 
+            // clmText4
+            // 
+            this.clmText4.FillWeight = 5F;
+            this.clmText4.HeaderText = "";
+            this.clmText4.Name = "clmText4";
+            this.clmText4.ReadOnly = true;
+            this.clmText4.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.clmText4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // clmCheck5
+            // 
+            this.clmCheck5.FillWeight = 5F;
+            this.clmCheck5.HeaderText = "";
+            this.clmCheck5.Name = "clmCheck5";
+            // 
+            // clmText5
+            // 
+            this.clmText5.FillWeight = 5F;
+            this.clmText5.HeaderText = "";
+            this.clmText5.Name = "clmText5";
+            this.clmText5.ReadOnly = true;
+            this.clmText5.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.clmText5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // clmCheck6
+            // 
+            this.clmCheck6.FillWeight = 5F;
+            this.clmCheck6.HeaderText = "";
+            this.clmCheck6.Name = "clmCheck6";
+            // 
+            // clmText6
+            // 
+            this.clmText6.FillWeight = 5F;
+            this.clmText6.HeaderText = "";
+            this.clmText6.Name = "clmText6";
+            this.clmText6.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose the numbers below";
+            // 
+            // btnAnotherBet
+            // 
+            this.btnAnotherBet.Location = new System.Drawing.Point(297, 326);
+            this.btnAnotherBet.Name = "btnAnotherBet";
+            this.btnAnotherBet.Size = new System.Drawing.Size(130, 23);
+            this.btnAnotherBet.TabIndex = 2;
+            this.btnAnotherBet.Text = "Another bet?";
+            this.btnAnotherBet.UseVisualStyleBackColor = true;
+            this.btnAnotherBet.Click += new System.EventHandler(this.btnAnotherBet_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnNext.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnNext.Location = new System.Drawing.Point(297, 326);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(135, 23);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // NumbersChosen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 365);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnAnotherBet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grdNumbers);
+            this.Name = "NumbersChosen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Numbers";
+            this.Load += new System.EventHandler(this.ChosenNumbers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdNumbers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAnotherBet;
+        private System.Windows.Forms.Button btnNext;                
+        private System.Windows.Forms.DataGridViewCheckBoxColumn clm1Check;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmText2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn clmCheck2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmText1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn clmCheck3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmText3;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn clmCheck4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmText4;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn clmCheck5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmText5;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn clmCheck6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmText6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView grdNumbers;
+    }
+}
---
+++ LoterySefaz/LoterySefaz/NumbersChosen.cs
@@ -0,0 +1,186 @@
+﻿using LoterySefaz.Core;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace LoterySefaz
+{
+    public partial class NumbersChosen : Form
+    {
+        private readonly Principal principalForm;
+
+        private int count;
+        private int betAmount = 1;
+
+        public List<List<int>> amountValues = new List<List<int>>();
+        public Dictionary<int, List<int>> numbers = new Dictionary<int, List<int>>();
+        public List<int> values = new List<int>();
+
+        public List<int> columnList = new List<int>();
+        
+        public NumbersChosen(Principal principalForm)
+        {
+            this.principalForm = principalForm;
+            InitializeComponent();
+        }
+
+        private void ChosenNumbers_Load(object sender, EventArgs e)
+        {
+            if (principalForm.player.AmountBetNumber > 1)
+            {
+                btnNext.Hide();
+
+                btnAnotherBet.Show();
+            }
+            else
+            {
+                btnNext.Show();
+
+                btnAnotherBet.Hide();
+            }
+
+            FillGrid();
+        }
+
+        private void FillGrid()
+        {
+            grdNumbers.Rows.Clear();
+
+            numbers = new Dictionary<int, List<int>>();
+
+            int j = 1;
+
+            for (int i = 1; i <= Constants.MegaSenaRange; i++)
+            {
+                columnList.Add(i);
+
+                if (columnList.Count == Constants.Sena)
+                {
+                    numbers.Add(j, columnList);
+                    columnList = new List<int>();
+
+                    j++;
+                }
+            }
+
+            foreach (var number in numbers)
+            {
+                int rowNum = grdNumbers.Rows.Add(new object[] 
+                       { null, number.Value[0],
+                        null, number.Value[1],
+                        null, number.Value[2],
+                        null, number.Value[3],
+                        null, number.Value[4],
+                        null, number.Value[5] });
+            }
+        }
+
+        private void FillValues()
+        {
+            foreach (DataGridViewRow line in grdNumbers.Rows)
+            {
+                foreach (DataGridViewColumn column in grdNumbers.Columns)
+                {
+                    if (grdNumbers.Rows[line.Index].Cells[column.Index].GetType() == typeof(DataGridViewCheckBoxCell)
+                        && Convert.ToBoolean(grdNumbers.Rows[line.Index].Cells[column.Index].Value) == true)
+                    {
+                        values.Add(Convert.ToInt32(grdNumbers.Rows[line.Index].Cells[column.Index + 1].Value));
+                    }
+                }
+            }
+        }
+
+        private void grdNumbers_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            count = 0;
+
+            foreach (DataGridViewRow linha in grdNumbers.Rows)
+            {
+                foreach (DataGridViewColumn column in grdNumbers.Columns)
+                {
+                    if (linha.Cells[column.Index].ValueType == typeof(bool))
+                    {
+                        //Check if the limit of possible numbers were hit
+                        if (count == 7)
+                        {
+                            grdNumbers.CurrentCell.Value = false;
+
+                            MessageBox.Show("Six numbers allowed.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                            return;
+                        }
+
+                        if (Convert.ToBoolean(linha.Cells[column.Index].Value) == true)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void grdNumbers_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            if (grdNumbers.IsCurrentCellDirty && grdNumbers.CurrentCell.GetType() == typeof(DataGridViewCheckBoxCell))
+            {
+                grdNumbers.EndEdit();
+            }
+        }
+
+        private void btnAnotherBet_Click(object sender, EventArgs e)
+        {
+            if (count < Constants.Sena)
+            {
+                MessageBox.Show("Choose six numbers", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            FillValues();
+
+            var bet = new Bet();
+            bet.BetId = betAmount;
+            bet.BetDate = DateTime.Now;
+            bet.Numbers = values;
+
+            principalForm.player.BetList.Add(bet);
+
+            values = new List<int>();
+
+            FillGrid();
+
+            if (betAmount == principalForm.player.AmountBetNumber - 1)
+            {
+                btnNext.Show();
+
+                btnAnotherBet.Hide();
+            }
+
+            betAmount++;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (count < Constants.Sena)
+            {
+                MessageBox.Show("Choose six numbers", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            FillValues();
+
+            var bet = new Bet();
+            bet.BetDate = DateTime.Now;
+            bet.Numbers = values;
+
+            principalForm.player.BetList.Add(bet);
+
+            var draw = new Draw(principalForm);
+
+            draw.Show();
+
+            Hide();
+        }
+    }
+}
