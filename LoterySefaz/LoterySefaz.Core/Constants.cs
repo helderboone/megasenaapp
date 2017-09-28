8f3dc720911089e90f172e4adb39302fb3f8fad5
Helder Boone <helder.boone@gmail.com>
2017-09-28 14:03:52 -0300

    Adiciona constantes ao sistema de loteria

---
+++ LoterySefaz/LoterySefaz.Core/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoterySefaz.Core
+{
+    public static class Constants
+    {
+        public const int MegaSenaRange = 60;
+        public const int Quadra = 4;
+        public const int Quina = 5;
+        public const int Sena = 6;
+    }
+}
