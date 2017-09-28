c108b41a322e90173bcd9e0e031907d12452e49e
Helder Boone <helder.boone@gmail.com>
2017-09-28 16:45:42 -0300

    Implementa a tela principal da loteria

---
+++ LoterySefaz/LoterySefaz/Principal.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace LoterySefaz
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkGenerateBet = new System.Windows.Forms.CheckBox();
+            this.txtBetAmount = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(66, 22);
+            this.txtName.MaxLength = 150;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(199, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkGenerateBet);
+            this.groupBox1.Controls.Add(this.txtBetAmount);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblName);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(668, 63);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fill the fields below:";
+            // 
+            // chkGenerateBet
+            // 
+            this.chkGenerateBet.AutoSize = true;
+            this.chkGenerateBet.Location = new System.Drawing.Point(495, 25);
+            this.chkGenerateBet.Name = "chkGenerateBet";
+            this.chkGenerateBet.Size = new System.Drawing.Size(174, 17);
+            this.chkGenerateBet.TabIndex = 2;
+            this.chkGenerateBet.Text = "Generate the bets automatically?";
+            this.chkGenerateBet.UseVisualStyleBackColor = true;
+            // 
+            // txtBetAmount
+            // 
+            this.txtBetAmount.Location = new System.Drawing.Point(414, 22);
+            this.txtBetAmount.Mask = "9999";
+            this.txtBetAmount.Name = "txtBetAmount";
+            this.txtBetAmount.Size = new System.Drawing.Size(31, 20);
+            this.txtBetAmount.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(300, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bet amount:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(22, 25);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name:";
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnNext.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnNext.Location = new System.Drawing.Point(269, 94);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(188, 31);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 147);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mega Sena might change your life forever";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txtBetAmount;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.CheckBox chkGenerateBet;
+    }
+}
---
+++ LoterySefaz/LoterySefaz/Principal.cs
@@ -0,0 +1,105 @@
+﻿using LoterySefaz.Core;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LoterySefaz
+{
+    public partial class Principal : Form
+    {
+        private Random rnd = new Random();
+        public Player player { get; set; }
+        public Bet bet { get; set; }
+        
+        public List<int> values = new List<int>();
+        public List<Bet> amountBets = new List<Bet>();
+
+        public Principal()
+        {
+            player = new Player();
+            bet = new Bet();
+            InitializeComponent();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            //Check if the fields are filled correctly   
+            if (string.IsNullOrEmpty(txtBetAmount.Text.Replace("_", "").Trim()) || string.IsNullOrEmpty(txtName.Text.Trim()) )
+            {
+                MessageBox.Show("Fill the fields are required", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            //Not allow amount of zero numbers chosen
+            if (Convert.ToInt32(txtBetAmount.Text) == 0)
+            {
+                MessageBox.Show("Can not be 0.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (chkGenerateBet.Checked)
+            {
+                var betAmount = 1;
+
+                while (player.BetList.Count < Convert.ToInt32(txtBetAmount.Text))
+                {
+                    bet = new Bet();
+
+                    values = new List<int>();
+
+                    var possibleNumbers = new List<int>();
+
+                    for (int i = 1; i <= Constants.MegaSenaRange; i++)
+                    {
+                        possibleNumbers.Add(i);
+                    }
+                        
+                    var drawnNumbersLoterry = from n in possibleNumbers orderby rnd.Next(possibleNumbers.Count) select n;
+
+                    foreach (var number in drawnNumbersLoterry)
+                    {
+                        if (values.Count == Constants.Sena)
+                        {
+                            break;
+                        }
+
+                        values.Add(number);
+                    }
+
+                    bet.BetId = betAmount;
+                    bet.Numbers = values;
+                    bet.BetDate = DateTime.Now;
+
+                    player.Name = txtName.Text;
+                    player.BetList.Add(bet);
+                    player.AmountBetNumber++;
+
+                    //Reset the values
+                    values = null;
+                    bet = null;
+                }
+
+
+                var draw = new Draw(this);
+
+                draw.Show();
+
+                Hide();
+            }
+            else
+            {
+                player.Name = txtName.Text;
+
+                player.AmountBetNumber = Convert.ToInt32(txtBetAmount.Text);
+
+                var numbersChosen = new NumbersChosen(this);
+
+                numbersChosen.Show();
+
+                Hide();
+            }
+        }
+    }
+}
