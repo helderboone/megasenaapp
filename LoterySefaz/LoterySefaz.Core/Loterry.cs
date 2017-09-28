e6fae3c36ca56c67fed0646abb9701bf2adaa8e6
Helder Boone <helder.boone@gmail.com>
2017-09-28 14:06:01 -0300

    Adiciona o contrato para ser utilizado tanto na mega sena quanto na lotomania

---
+++ LoterySefaz/LoterySefaz.Core/Loterry.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace LoterySefaz.Core
+{
+    public abstract class Loterry
+    {
+        public List<int> DrawnNumbers { get; set; }
+
+        public abstract Dictionary<string, string> Draw(Player player);
+    }
+}
