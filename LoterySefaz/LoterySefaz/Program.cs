6774dc0d72c11d841d8094e693e183469b4970c2
Helder Boone <helder.boone@gmail.com>
2017-09-28 13:59:36 -0300

    Coloca a classe Principal.cs como classe padrao

--- LoterySefaz/LoterySefaz/Program.cs
+++ LoterySefaz/LoterySefaz/Program.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LoterySefaz
 {
-    static class Program
+    public static class Program
     {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Principal());
         }
     }
 }
