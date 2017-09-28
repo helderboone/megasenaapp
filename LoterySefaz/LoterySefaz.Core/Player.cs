2ee16eebe0c4a5844008a8a4a79208d1d4aa5ca5
Helder Boone <helder.boone@gmail.com>
2017-09-27 21:30:57 -0300

    Cria a classe do jogador

---
+++ LoterySefaz/LoterySefaz.Core/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LoterySefaz.Core
+{
+    public class Player
+    {
+        public Player()
+        {
+            BetList = new List<Bet>();
+        }
+
+        public string Name { get; set; }
+
+        public int AmountBetNumber { get; set; }
+
+        public List<Bet> BetList { get; set; }
+    }
+}
