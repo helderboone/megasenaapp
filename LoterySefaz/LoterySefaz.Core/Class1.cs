af03af942478049be14a5af9f8f49f7fd57efbfc
Helder Boone <helder.boone@gmail.com>
2017-09-27 21:15:48 -0300

    Commit inicial do projeto

---
+++ LoterySefaz/LoterySefaz.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoterySefaz.Core
+{
+    public class Class1
+    {
+    }
+}
